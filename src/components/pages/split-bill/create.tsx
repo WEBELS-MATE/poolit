86339d9622e602654027b02d8323820d4f6a6f8d
Xeyla Vithra Arfhina <xeyla@xeyla.xeyla>
2025-07-23 22:05:44 +0700

    choose bill option

--- src/components/pages/split-bill/create.tsx
+++ src/components/pages/split-bill/create.tsx
@@ -5,7 +5,10 @@ import Box from '../../../assets/box.png';
 export default function reate() {
   return (
     <MainLayout>
-      <div className="grid grid-cols-1 md:grid-cols-2  h-screen w-full place-items-center">
+      <div
+        style={{ fontFamily: 'Namco Regular' }}
+        className="grid grid-cols-1 md:grid-cols-2 text-[#] h-screen w-full place-items-center"
+      >
         <div
           className="w-150 flex flex-col h-150 bg-[linear-gradient(224deg,_#BA2685_-2.35%,_#54113C_96.44%)] shadow-lg"
           style={{
@@ -18,9 +21,9 @@ export default function reate() {
             alt="Box Pencil"
             className="w-1/2 h-1/2 mr-10 mt-20 self-end"
           />
-          <h2 className="ml-10 mt-10 text-5xl text-white text-start">INPUT</h2>
-          <h2 className="ml-10 mt-3 text-5xl text-white text-start">
-            TOTAL AMOUNT
+          <h2 className="ml-10 mt-10 text-3xl text-white text-start">input</h2>
+          <h2 className="ml-10 mt-3 text-3xl text-white text-start">
+            total amount
           </h2>
         </div>
         <div
@@ -35,9 +38,9 @@ export default function reate() {
             alt="Box"
             className="w-1/2 h-1/2 mr-10 mt-20 self-end"
           />
-          <h2 className="ml-10 mt-10 text-5xl text-white text-start">INPUT</h2>
-          <h2 className="ml-10 mt-3 text-5xl text-white text-start">
-            AMOUNT ITEM
+          <h2 className="ml-10 mt-10 text-3xl text-white text-start">input</h2>
+          <h2 className="ml-10 mt-3 text-3xl text-white text-start">
+            amount item
           </h2>
         </div>
       </div>
