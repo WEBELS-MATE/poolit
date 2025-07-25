5a02613b1e9ebfe7fc79f59cbe9c6fb44a5e5f74
ZeroZennn <achmad.zikran@gmail.com>
2025-07-25 14:13:43 +0700

    Creating Dashboard Fe

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from '../ui/sidebar';
 import Topbar from '../ui/topbar';
+import PatternBg from '../../assets/bg-pattern.png';
 
 export default function MainLayout({
   children,
@@ -8,11 +9,16 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-row w-full h-screen bg-white">
-      <div className='flex-1'>
+    <div
+      className="flex flex-row w-full h-screen bg-white bg-repeat"
+      style={{
+        backgroundImage: `url(${PatternBg})`,
+      }}
+    >
+      <div className="flex-1">
         <Sidebar />
       </div>
-      <div className="flex-5 min-h-screen flex flex-col overflow-hidden">
+      <div className="flex-5 min-h-screen flex flex-col overflow-hidden bg-transparent">
         <Topbar />
         <div className="flex-1 overflow-y-auto pt-16 ps-6 pb-6">
           {children}
---
+++ src/components/pages/split-bill/create.tsx
@@ -0,0 +1,49 @@
+import MainLayout from '../../layout/MainLayout';
+import BoxPencil from '../../../assets/box-pencil.png';
+import Box from '../../../assets/box.png';
+
+export default function reate() {
+  return (
+    <MainLayout>
+      <div
+        style={{ fontFamily: 'Namco Regular' }}
+        className="grid grid-cols-1 md:grid-cols-2 text-[#] h-screen w-full place-items-center"
+      >
+        <div
+          className="w-150 flex flex-col h-150 bg-[linear-gradient(224deg,_#BA2685_-2.35%,_#54113C_96.44%)] shadow-lg"
+          style={{
+            clipPath:
+              'polygon(200px 0%, 150% 0%, 150% 150%, 0% 150%, 0% 200px)',
+          }}
+        >
+          <img
+            src={BoxPencil}
+            alt="Box Pencil"
+            className="w-1/2 h-1/2 mr-10 mt-20 self-end"
+          />
+          <h2 className="ml-10 mt-10 text-3xl text-white text-start">input</h2>
+          <h2 className="ml-10 mt-3 text-3xl text-white text-start">
+            total amount
+          </h2>
+        </div>
+        <div
+          className="w-150 flex flex-col h-150 bg-[#BA2685] shadow-lg"
+          style={{
+            clipPath:
+              'polygon(200px 0%, 150% 0%, 150% 150%, 0% 150%, 0% 200px)',
+          }}
+        >
+          <img
+            src={Box}
+            alt="Box"
+            className="w-1/2 h-1/2 mr-10 mt-20 self-end"
+          />
+          <h2 className="ml-10 mt-10 text-3xl text-white text-start">input</h2>
+          <h2 className="ml-10 mt-3 text-3xl text-white text-start">
+            amount item
+          </h2>
+        </div>
+      </div>
+    </MainLayout>
+  );
+}
